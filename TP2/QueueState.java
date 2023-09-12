1ea5a24f328fe1de5e9186bc13769a6a249af260
TAVuwu <130508589+TAVuwu@users.noreply.github.com>
2023-09-12 18:58:18 -0300

    Update QueueState.java

--- TP2/QueueState.java
+++ TP2/QueueState.java
@@ -1,9 +1,6 @@
 package queue;
 
 public abstract class QueueState {
-    public abstract boolean isEmpty();
-    public abstract QueueState add(Object cargo);
     public abstract Object take();
     public abstract Object head();
-    public abstract int size();
 }
