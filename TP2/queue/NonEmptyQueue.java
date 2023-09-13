130a0b0566769ebb53037dee769227c26766523a
Ticiana Verdun <130508589+TAVuwu@users.noreply.github.com>
2023-09-13 17:50:39 -0300

    Update NonEmptyQueue.java

--- TP2/queue/NonEmptyQueue.java
+++ TP2/queue/NonEmptyQueue.java
@@ -4,18 +4,18 @@ public class NonEmptyQueue extends QueueState{
 	
 	public Object object;
 	
-public NonEmptyQueue (Object cargo) {
-	object = cargo;
-}
-
-@Override
-public Object take() {
-	return object;
-}
-
-@Override
-public Object head() {
+	public NonEmptyQueue (Object cargo) {
+		object = cargo;
+	}
+	
+	@Override
+    public Object take() {
         return object;
-}
+    }
+    
+    @Override
+    public Object head() {
+        return object;
+    }
    
 }
