358745cd99ec9d8b42534b85554053c968f43cd6
Ticiana Verdun <130508589+TAVuwu@users.noreply.github.com>
2023-09-13 17:51:17 -0300

    Update EmptyQueue.java

--- TP2/queue/EmptyQueue.java
+++ TP2/queue/EmptyQueue.java
@@ -5,12 +5,12 @@ public class EmptyQueue extends QueueState{
 	public static final String ErrorMessage_QueueIsEmpty = "Queue is empty";
 
 	@Override
-    public Object take() {
-        throw new Error (ErrorMessage_QueueIsEmpty);
-    }
+    	public Object take() {
+        	throw new Error (ErrorMessage_QueueIsEmpty);
+   	}
 	
 	@Override
-    public Object head() {
-    	throw new Error (ErrorMessage_QueueIsEmpty);
-    }
+    	public Object head() {
+    		throw new Error (ErrorMessage_QueueIsEmpty);
+    	}
 }
