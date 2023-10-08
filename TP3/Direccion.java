ee081d4f8770bb9e2ff50cc4579295bc2e423267
Ticiana Verdun <130508589+TAVuwu@users.noreply.github.com>
2023-10-08 15:51:15 -0300

    Create Direccion.java

---
+++ TP3/Direccion.java
@@ -0,0 +1,10 @@
+package sumergible;
+
+import java.util.List;
+
+public abstract class Direccion {
+	public abstract Direccion left ();
+	public abstract Direccion right ();
+	public abstract List<Integer> forward (int coordenadaX, int coordenadaY);
+	public abstract String getDireccion ();
+}
