6b815dda21039d5bb01a4f9ec49265a61e12734b
Ticiana Verdun <130508589+TAVuwu@users.noreply.github.com>
2023-10-17 19:18:03 -0300

    Create Profundidad.java

---
+++ TP3/Profundidad.java
@@ -0,0 +1,9 @@
+package nemo;
+
+import java.util.List;
+
+public abstract class Profundidad {
+	public abstract Profundidad subirNemo ();
+	public abstract Profundidad bajarNemo ();
+	public abstract boolean liberarCapsula ();
+}
