b45a46626d2f8d8e6a3881c159d6bfff900d8f0c
Ticiana Verdun <130508589+TAVuwu@users.noreply.github.com>
2023-10-20 11:49:51 -0300

    Delete TP3 directory

--- TP3/ComandoNemo.java
+++
@@ -1,9 +0,0 @@
-package nemo;
-
-public abstract class ComandoNemo {
-    protected char comando;
-
-    public abstract boolean compararComando(char c);
-    public abstract void ejecutar(Nemo nemo);
-
-}
--- TP3/Profundidad.java
+++
@@ -1,9 +0,0 @@
-package nemo;
-
-import java.util.List;
-
-public abstract class Profundidad {
-	public abstract Profundidad subirNemo ();
-	public abstract Profundidad bajarNemo ();
-	public abstract boolean liberarCapsula ();
-}
