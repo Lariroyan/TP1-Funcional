9ec00e1d9e0269459ab2bb330cc77b7620b14069
Ticiana Verdun <130508589+TAVuwu@users.noreply.github.com>
2023-11-12 16:19:06 -0300

    Create Game.java

---
+++ TP4/fourInARow/Game.java
@@ -0,0 +1,37 @@
+package fourInARow;
+
+public class Game {
+
+	public static void main( String[] args) throws Exception {
+
+	    System.out.println( "Dimensiones?");
+
+	    Linea game = new Linea( promptAsInt( "Base? " ),
+	                            promptAsInt( "Altura? " ),
+	                            promptAsChar( "Estrategia de Juego: A, B o C? " ) );
+
+		System.out.println( "Ingrese desde 0 (para primera columna) hasta " + (game.getBase() - 1) + " (para ultima columna) para colocar una ficha" );
+
+	    System.out.println( game.show() );
+
+	    while ( !game.finished() ) {
+	      	game.playRedAt( promptAsInt( "Rojas? " ) );
+	      	System.out.println( game.show() );
+	      
+	      	if ( !game.finished() ) {
+			  	game.playBlueAt( promptAsInt( "Azul? " ) );
+			  	System.out.println( game.show() );
+		  	}
+	    }
+	}
+
+	private static int promptAsInt( String message ) {
+		System.out.print( message );
+	    return Integer.parseInt( System.console().readLine() );
+	}
+
+	private static char promptAsChar( String message ) {
+	    System.out.print( message );
+	    return System.console().readLine().charAt( 0 );
+	}
+}
