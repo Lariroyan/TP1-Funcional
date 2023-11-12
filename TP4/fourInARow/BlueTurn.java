2afb12a8d3855ffe9f02996ad016b5a0ff5903b8
Ticiana Verdun <130508589+TAVuwu@users.noreply.github.com>
2023-11-12 14:58:32 -0300

    Create BlueTurn.java

---
+++ TP4/fourInARow/BlueTurn.java
@@ -0,0 +1,12 @@
+package fourInARow;
+
+public class BlueTurn extends Turn {
+    public Turn playRed(Linea linea, int column) {
+        throw new RuntimeException(messageNotRedsTurn);
+    }
+
+    public Turn playBlue(Linea linea, int column) {
+        linea.play('B', column);
+        return new RedTurn();
+    }
+}
